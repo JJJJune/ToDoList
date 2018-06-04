79f89d36cf55612b90302a9586facf4593dc4abd
zheng <zhen.wang@target-energysolutions.com>
2018-06-04 10:26:07 +0800

    [feat] completed footer and button

--- todoapp/src/components/app-footer/index.js
+++ todoapp/src/components/app-footer/index.js
@@ -2,17 +2,31 @@ import React, { Component} from 'react'
 import './styles.css'
 
 export default class Footer extends Component {
+    handleShowAll = () => {
+        const {ShowAll} = this.props
+        ShowAll()
+    }
+    handleShowActive = () => {
+        const {ShowActive} = this.props
+        ShowActive()
+    }
+    handleShowCompleted = () => {
+        const {ShowCompleted} = this.props
+        ShowCompleted()
+    }
+    handleDestroyCompleted = () => {
+        const {ClearCompleted} = this.props
+        ClearCompleted()
+    }
     render() {
         const {leftitems} = this.props
         return(
             <div className="app-footer">
                 <span>{leftitems} items left</span>
-                <label>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </label>
-                <button>Clear Completed</button>
+                <button onClick={this.handleShowAll}>All</button>
+                <button onClick={this.handleShowActive}>Active</button>
+                <button onClick={this.handleShowCompleted}>Completed</button>
+                <button onClick={this.handleDestroyCompleted}>Clear Completed</button>
             </div>
         );
     }
--- todoapp/src/components/app/index.js
+++ todoapp/src/components/app/index.js
@@ -2,55 +2,78 @@ import React, { Component } from 'react'
 import Header from '../app-header'
 import TodoItem from '../app-todoitem'
 import Footer from '../app-footer'
-var id=0;
-
+var id=0
+var TodoList = []
+var n=0
 export default class todoapp extends Component {
     state = {
         completed: false,
-        leftitems: 0,
         todolist:[]
     }
     handleAddTodo = (newtodo) => {
+        TodoList = [...this.state.todolist, {id:id++,title:newtodo, completed: false}]
+        n++
         this.setState ({
-            todolist: [...this.state.todolist, {id:id++,title:newtodo, completed: false}]
+            todolist: TodoList
         })
-        this.handleLeftitems()
-        console.log("增加一件事后的数组：", this.state.todolist)
     }
     handleDelete = (id) => {
-        this.handleLeftitems()
+        TodoList = this.state.todolist.filter( todo => {
+            if(todo.id !== id) {
+                return todo
+            }
+            if(todo.id === id && todo.completed === false)
+            {
+                n--
+            }
+        })
         this.setState({
-            todolist: this.state.todolist.filter((n) =>  n.id !== id)
+            todolist: TodoList
         });
     }
     hanleCompleted = (id) => {
-       const todolist = this.state.todolist
-       const newtodolist = todolist.map( (n) => {
-           if(n.id === id){
-                n.completed = !n.completed
-               return n
+       TodoList = this.state.todolist.map( todo => {
+           if(todo.id === id){
+                todo.completed = !todo.completed
+                if(todo.completed === true){
+                    n--
+                }
+                else n++
+               return todo
            }else 
-            return n
+            return todo
        })
        this.setState({
-           todolist: newtodolist
+           todolist: TodoList
        })
-       //console.log(newtodolist)
     }
-    handleLeftitems = () => {
-        const todolist = this.state.todolist
-        let n=0;
-        for(let todo of todolist){
-            if(!todo.completed){
-                n++;
-            }
-        }
+    handleShowAll = () => {
         this.setState({
-            leftitems: n
+            todolist: TodoList
         })
     }
+    handleShowActive = () => {
+        this.setState({
+            todolist: TodoList.filter( todo => !todo.completed)
+        })
+    }
+    handleShowCompleted = () => {
+        this.setState({
+            todolist: TodoList.filter( todo => todo.completed)
+        })
+    }
+    handleClearCompleted = () => {
+        TodoList = TodoList.filter(todo =>  {
+            if(todo.completed === false){
+                return todo
+            }
+        })
+        this.setState({
+            todolist: TodoList
+        });
+    }
     render() {
-        const {leftitems, todolist} = this.state
+        const {todolist} = this.state
         const todos = todolist.map(({id,title,completed}) => 
                 (<TodoItem 
                     key={id}
@@ -64,7 +87,12 @@ export default class todoapp extends Component {
                 <h1>todos</h1>
                 <Header onEnter={this.handleAddTodo}/>
                 {todos}
-                <Footer leftitems={leftitems}/>
+                <Footer 
+                    leftitems={n} 
+                    ShowAll={this.handleShowAll}
+                    ShowActive={this.handleShowActive}
+                    ShowCompleted={this.handleShowCompleted}
+                    ClearCompleted={this.handleClearCompleted}/>
             </div>
         );
     }
